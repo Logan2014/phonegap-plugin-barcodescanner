c25aea7063067221c718e408d2fdae559de14352
Ovcharenko Vladimir <v.ovcharenko@market-universal.com.ua>
2016-08-11 11:34:37 +0300

    Fix bug with cordova wrap

--- src/windows/BarcodeScannerProxy.js
+++ src/windows/BarcodeScannerProxy.js
@@ -1,5 +1,4 @@
-﻿cordova.define("phonegap-plugin-barcodescanner.BarcodeScannerProxy", function(require, exports, module) {
-/*
+﻿/*
  * Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
@@ -647,5 +646,3 @@ module.exports = {
 };
 
 require("cordova/exec/proxy").add("BarcodeScanner", module.exports);
-
-});
